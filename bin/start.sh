04b29421e5e9c8bde7c2e2a407d87fe5a4a7973b
huggla <henrik.uggla@kristianstad.se>
2018-02-01 11:26:29 +0100

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -10,16 +10,16 @@ then
    do
       echo "$db" >> "$CONFIG_FILE"
    done
-   echo
+   echo >> "$CONFIG_FILE"
    echo "[pgbouncer]" >> "$CONFIG_FILE"
-   echo "listen_addr = \"$LISTEN_ADDR\"" >> "$CONFIG_FILE"
-   echo "auth_file = \"$AUTH_FILE\""  >> "$CONFIG_FILE"
+   echo "listen_addr = $LISTEN_ADDR" >> "$CONFIG_FILE"
+   echo "auth_file = $AUTH_FILE"  >> "$CONFIG_FILE"
    if [ -n "$AUTH_HBA_FILE" ]
    then
       echo "auth_type = hba" >> "$CONFIG_FILE"
-      echo "auth_hba_file = \"$AUTH_HBA_FILE\""  >> "$CONFIG_FILE"
+      echo "auth_hba_file = $AUTH_HBA_FILE"  >> "$CONFIG_FILE"
    fi
-   echo "unix_socket_dir = \"$UNIX_SOCKET_DIR\"" >> "$CONFIG_FILE"
+   echo "unix_socket_dir = $UNIX_SOCKET_DIR" >> "$CONFIG_FILE"
    for conf in $ADDITIONAL_CONFIGURATION
    do
       echo "$conf" >> "$CONFIG_FILE"
