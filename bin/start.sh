3c0c45cae43a82f22877e1db20f3d018efe66b80
huggla <henrik.uggla@kristianstad.se>
2018-02-16 15:10:36 +0100

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -9,34 +9,33 @@ if [ -d "$SUDO_DIR" ]
 then
    IFS="${IFS};"
    sudo="/usr/bin/sudo"
-
-if [ ! -s "$CONFIG_FILE" ]
-then
-   echo "[databases]" > "$CONFIG_FILE"
-   for db in $DATABASES
-   do
-      echo "$db" >> "$CONFIG_FILE"
-   done
-   echo >> "$CONFIG_FILE"
-   echo "[pgbouncer]" >> "$CONFIG_FILE"
-   echo "listen_addr=$LISTEN_ADDR" >> "$CONFIG_FILE"
-   echo "auth_file=$AUTH_FILE" >> "$CONFIG_FILE"
-   if [ -n "$AUTH_TYPE" ]
+   if [ ! -s "$CONFIG_FILE" ]
    then
-      echo "auth_type=$AUTH_TYPE" >> "$CONFIG_FILE"
-      if [ ! `echo $AUTH_TYPE | grep -iq "hba"` ]
+      echo "[databases]" > "$CONFIG_FILE"
+      for db in $DATABASES
+      do
+         echo "$db" >> "$CONFIG_FILE"
+      done
+      echo >> "$CONFIG_FILE"
+      echo "[pgbouncer]" >> "$CONFIG_FILE"
+      echo "listen_addr=$LISTEN_ADDR" >> "$CONFIG_FILE"
+      echo "auth_file=$AUTH_FILE" >> "$CONFIG_FILE"
+      if [ -n "$AUTH_TYPE" ]
       then
-         echo "auth_hba_file=$AUTH_HBA_FILE" >> "$CONFIG_FILE"
+         echo "auth_type=$AUTH_TYPE" >> "$CONFIG_FILE"
+         if [ ! `echo $AUTH_TYPE | grep -iq "hba"` ]
+         then
+            echo "auth_hba_file=$AUTH_HBA_FILE" >> "$CONFIG_FILE"
+         fi
       fi
-   fi
-   echo "unix_socket_dir=$UNIX_SOCKET_DIR" >> "$CONFIG_FILE"
-   if [ -n "$CLIENT_TLS_SSLMODE" ] && [ `echo $CLIENT_TLS_SSLMODE | grep -iq "disable"` ]
-   then
-      echo "client_tls_sslmode=$CLIENT_TLS_SSLMODE" >> "$CONFIG_FILE"
-      echo "client_tls_ca_file=$CLIENT_TLS_CA_FILE" >> "$CONFIG_FILE"
-      echo "client_tls_cert_file=$CLIENT_TLS_CERT_FILE" >> "$CONFIG_FILE"
-      echo "client_tls_key_file=$CLIENT_TLS_KEY_FILE" >> "$CONFIG_FILE"
-   fi
+      echo "unix_socket_dir=$UNIX_SOCKET_DIR" >> "$CONFIG_FILE"
+      if [ -n "$CLIENT_TLS_SSLMODE" ] && [ `echo $CLIENT_TLS_SSLMODE | grep -iq "disable"` ]
+      then
+         echo "client_tls_sslmode=$CLIENT_TLS_SSLMODE" >> "$CONFIG_FILE"
+         echo "client_tls_ca_file=$CLIENT_TLS_CA_FILE" >> "$CONFIG_FILE"
+         echo "client_tls_cert_file=$CLIENT_TLS_CERT_FILE" >> "$CONFIG_FILE"
+         echo "client_tls_key_file=$CLIENT_TLS_KEY_FILE" >> "$CONFIG_FILE"
+      fi
    if [ -n "$SERVER_RESET_QUERY" ]
    then
       echo "server_reset_query=$SERVER_RESET_QUERY" >> "$CONFIG_FILE"
