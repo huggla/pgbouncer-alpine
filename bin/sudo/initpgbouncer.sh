26bb0bda547511ba27e473298d5d0a5f6a06e947
huggla <henrik.uggla@kristianstad.se>
2018-02-26 15:32:23 +0100

    Update initpgbouncer.sh

--- bin/sudo/initpgbouncer.sh
+++ bin/sudo/initpgbouncer.sh
@@ -104,10 +104,10 @@ then
          makefile "$userpwfile"
          if [ ! -s "$userpwfile" ]
          then
-            user_pw="$(var - password_$user_lc | /usr/bin/head -c -1)"
+            user_pw="$(var - password_$user_lc)"
             if [ -n "$user_pw" ]
             then
-               echo $user_pw > $userpwfile
+               echo -n "$user_pw" > $userpwfile
                unset user_pw
             else
                echo "No password given for $user."
