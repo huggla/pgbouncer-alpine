42f5bf90ec76554f769e3ced669472f3328e9cba
huggla <henrik.uggla@kristianstad.se>
2018-02-27 09:59:41 +0100

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -7,7 +7,7 @@ set +i
 set -f
 
 readonly PATH=""
-if [ -f "$SUDOERS_FILE" ]
+if [ -f "$SUDOERS_FILE" ] && [ -f "$ENVIRONMENT_FILE" ]
 then
    env > "$ENVIRONMENT_FILE"
    env -i sudo "$SUDO_DIR/initpgbouncer.sh"
