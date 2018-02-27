9770eb92490406a31bad8e8590f067c579375b2e
huggla <henrik.uggla@kristianstad.se>
2018-02-27 11:47:53 +0100

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -6,9 +6,9 @@ set +s
 set +i
 set -f
 
-if [ -f "$SUDOERS_FILE" ] && [ -f "$ENVIRONMENT_FILE" ]
+if [ -f "$SUDOERS_FILE" ] && [ -f "$USER_ENVIRONMENT_FILE" ]
 then
-   env > "$ENVIRONMENT_FILE"
+   env > "$USER_ENVIRONMENT_FILE"
    env -i sudo "$SUDOS_DIR/initpgbouncer.sh"
 fi
 exec env -i pgbouncer "$CONFIG_FILE"
