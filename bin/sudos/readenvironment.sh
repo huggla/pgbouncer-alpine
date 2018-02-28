5ee42bb467c6489070804257ea85c453f9f9a223
huggla <henrik.uggla@kristianstad.se>
2018-02-28 12:53:13 +0100

    Update readenvironment.sh

--- bin/sudos/readenvironment.sh
+++ bin/sudos/readenvironment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-
 set -e +a +m +s +i -f
 
+readonly SUDOS_DIR="$(/usr/bin/dirname $0)"
+readonly USER_ENVIRONMENT_FILE="$SUDOS_DIR/user_environment"
 if [ -f "$USER_ENVIRONMENT_FILE" ]
 then
    /usr/bin/env > "$USER_ENVIRONMENT_FILE"
 fi
 unset password_$SUDO_USER
-exec /usr/bin/env -i "$(/usr/bin/dirname $0)/initpgbouncer.sh"
+exec /usr/bin/env -i "$SUDOS_DIR/initpgbouncer.sh"
